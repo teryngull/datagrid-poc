f24d1f005e917b597bf9a3ed8c9a86fb9b2427d8
Teryn Gull <teryngull@me.com>
2022-03-25 16:34:52 -0600

    add ag grid notes

--- src/components/aggridnotes.tsx
+++ src/components/aggridnotes.tsx
@@ -10,10 +10,16 @@ const AgGridNotes = (): JSX.Element => {
             <Typography variant='h5'>Overview:</Typography>
             <ul>
                 <li>
+                    I spent twice as long trying to set up a basic server side data grid with AG Grid than with MUI. This is due to them requiring a really weird &quot;getRows&quot; function to be set up, instead of just utilizing normal React type code.
+                </li>
+                <li>
                     They do not prioritize providing TypeScript examples, so I had to dig into the interfaces a lot, to only find that they have something typed very loosely, often as &quot;any&quot;.
                 </li>
                 <li>
-                    By default, every function of a data grid is off by default, so it takes a lot of extra work to turn everything on. As opposed to MUI where a lot of basic functionality is there from the start and you can choose to turn it off if need be.
+                    Lots of other props were typed as strings, but had specific values they had to be typed to. Which was either documented on their website or in some cases, was not.
+                </li>
+                <li>
+                    By default, every feature of a data grid is off by default, so it takes a lot of extra work to turn everything on. As opposed to MUI where a lot of basic functionality is there from the start and you can choose to turn it off if need be.
                 </li>
             </ul>
 
@@ -25,29 +31,23 @@ const AgGridNotes = (): JSX.Element => {
                         <li>
                             Used data types of strings, booleans, date and times
                         </li>
-                        <li>
-                            Simulated calling the server to sort and the same can be done for filtering and pagination
-                        </li>
                     </ul>
                 </li>
 
                 <li>
                     <b>Loading</b>
-                    {/* <ul>
+                    <ul>
                         <li>
-                            The loading component can be overriden from the default circular progress to a linear progress if wanted for a simpler look.
+                            The loading component can be overriden if we want.
                         </li>
-                    </ul> */}
+                    </ul>
                 </li>
 
                 <li>
                     <b>Columns</b>
-                    {/* <ul>
+                    <ul>
                         <li>
                             Added a custom actions column for our row actions
-                        </li>
-                        <li>
-                            Descriptions can be added for additional info when hovering over a column header
                         </li>
                         <li>
                             Column widths can be set to a number, or you can use a flex property to take up the remaining width, which is really cool
@@ -55,134 +55,129 @@ const AgGridNotes = (): JSX.Element => {
                         <li>
                             Cells can be rendered with custom components and lots of flexibility there for all use cases I could see
                         </li>
-                    </ul> */}
+                        <li>
+                            Column types are pretty confusing and not as simple as MUI to setup. There are ways to fully customize this, but it was difficult to understand the documentation
+                        </li>
+                    </ul>
                 </li>
 
                 <li>
                     <b>Pinned Columns</b>
-                    {/* <ul>
+                    <ul>
                         <li>
-                            Right now, I have the actions and checkbox columns pinned on the left
+                            Right now, I have the actions pinned on the left
                         </li>
-                    </ul> */}
+                    </ul>
                 </li>
 
                 <li>
                     <b>Sorting</b>
-                    {/* <ul>
+                    <ul>
                         <li>
-                            Implemented a fake server side sorting as an example
+                            Client side sorting is still enabled, but server side appears possible with more work
                         </li>
-                        <li>
-                            Sorting order is overridden to not allow them to &lsquo;clear&rsquo; sorting since we typically have a default sort order from the API
-                        </li>
-                    </ul> */}
+                    </ul>
                 </li>
 
                 <li>
                     <b>Filtering</b>
-                    {/* <ul>
+                    <ul>
                         <li>
-                            Client side filtering is still enabled, but server side is definitely possible and is fully controllable.
+                            Client side filtering is still enabled, but server side appears possible with more work
                         </li>
-                    </ul> */}
+                    </ul>
                 </li>
 
                 <li>
                     <b>Pagination</b>
-                    {/* <ul>
+                    <ul>
                         <li>
-                            Client side pagination is still enabled, but server side is definitely possible and is fully controllable.
+                            Client side pagination is still enabled, but server side appears possible with more work
                         </li>
-                    </ul> */}
+                    </ul>
                 </li>
 
                 <li>
                     <b>Master Detail</b>
-                    {/* <ul>
+                    <ul>
                         <li>
-                            Implemented a basic master detail expansion panel to show more details.
+                            Implemented a basic master detail expansion panel to show more details, but I was unable to get a working example
                         </li>
-                    </ul> */}
+                    </ul>
                 </li>
 
                 <li>
                     <b>Toolbar</b>
-                    {/* <ul>
+                    <ul>
                         <li>
-                            Can easily just use their default toolbar, or fully customize (see issues below)
+                            They do not seem to have any built-in toolbar, so we would have to implement anything ourselves
                         </li>
-                    </ul> */}
+                    </ul>
                 </li>
 
                 <li>
                     <b>Exporting</b>
-                    {/* <ul>
+                    <ul>
                         <li>
-                            Exporting is added in the default toolbar, and some customization is allowed (see issues below)
+                            Exporting is available with extra work to add the buttons and functionality
                         </li>
-                    </ul> */}
+                    </ul>
                 </li>
 
                 <li>
                     <b>Styling</b>
-                    {/* <ul>
+                    <ul>
                         <li>
-                            Some additional TS work is needed to be able to override the theme options for the data grid, but honestly the default styles look just fine.
+                            Default Material styles do not really line up with other MUI tables, but could be adjusted to our needs with SCSS.
                         </li>
-                    </ul> */}
+                    </ul>
                 </li>
             </ul>
 
             <Typography variant='h5'>Issues:</Typography>
             <ul>
                 <li>
-                    <b>Loading</b>
-                    {/* <ul>
-                        <li>
-                            Additional work/overrides would need to be done to use skeleton loaders, but I do not think that is a dealbreaker.
-                        </li>
-                    </ul> */}
-                </li>
-                <li>
                     <b>Columns</b>
-                    {/* <ul>
+                    <ul>
                         <li>
-                            If you implement `checkboxSelection`, I do not see a way to only allow one row to be selected.
+                            The checkbox and master detail options have to be tied to a column, which is kind of weird.
                         </li>
-                    </ul> */}
+                        <li>
+                            Ran into conflict issues with using server data and adding a select all checkbox in the header.
+                        </li>
+                    </ul>
                 </li>
                 <li>
                     <b>Pagination</b>
-                    {/* <ul>
+                    <ul>
                         <li>
-                            Default pagination is the slimmed down version for a table, but you can fully override if if you want to have the page selection as well.
+                            Pagination does seem to have some customization, but we may end up having to override the entire component to do anything other than the basics.
                         </li>
-                    </ul> */}
+                    </ul>
                 </li>
                 <li>
                     <b>Master Detail</b>
-                    {/* <ul>
+                    <ul>
                         <li>
-                            They might lack some customization with the master detail implementation, but I think for our needs it is probably fine.
+                            They might lack some customization with the master detail implementation, but it was hard to tell with their documenation.
                         </li>
-                    </ul> */}
+                    </ul>
                 </li>
                 <li>
                     <b>Toolbar</b>
-                    {/* <ul>
+                    <ul>
                         <li>
-                            Built-in Toolbar does not adjust for mobile, so it does not fully fit on the screen, so we would need to customize things there.
+                            No built in toolbar. Any examples of this are with them building custom html buttons to do any functions.
                         </li>
-                    </ul> */}
+                    </ul>
                 </li>
                 <li>
                     <b>Exporting</b>
-                    {/* <ul>
+                    <ul>
                         <li>
                             Exporting does not tie to a server, so we would maybe need to implement a custom export button instead.
                         </li>
-                    </ul> */}
+                    </ul>
                 </li>
                 <li>
                     <b>Themeing</b>
